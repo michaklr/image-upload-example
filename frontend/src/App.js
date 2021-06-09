fde52d29064901819da273bf0fa2dad28420b965
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-06-09 15:16:05 +0200

    Add Frontend Image Upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,45 @@
 import styled from 'styled-components/macro'
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 import axios from 'axios'
-import { AppHeader } from './components/AppHeader'
-import { ImageUpload } from './components/ImageUpload'
-import { Gallery } from './components/Gallery'
+import {AppHeader} from './components/AppHeader'
+import {ImageUpload} from './components/ImageUpload'
+import {Gallery} from './components/Gallery'
 
 export default function App() {
-  const [photos, setPhotos] = useState([])
-  const [status, setStatus] = useState('loading')
-  const [error, setError] = useState()
+    const [photos, setPhotos] = useState([])
+    const [status, setStatus] = useState('loading')
+    const [error, setError] = useState()
 
-  useEffect(() => {
-    setStatus('loading')
-    axios
-      .get('photo')
-      .then(res => res.data)
-      .then(setPhotos)
-      .then(() => setStatus('success'))
-      .catch(error => {
-        setError(error)
-        setStatus('error')
-      })
-  }, [])
+    const loadPhotos = () => {
+        setStatus('loading')
+        axios
+            .get('photo')
+            .then(res => res.data)
+            .then(setPhotos)
+            .then(() => setStatus('success'))
+            .catch(error => {
+                setError(error)
+                setStatus('error')
+            })
+    }
 
-  const isLoading = status === 'loading'
-  const isError = status === 'error'
-  const isSuccess = status === 'success'
+    useEffect(() => {
+        loadPhotos()
+    }, [])
 
-  return (
-    <Wrapper>
-      <AppHeader />
-      {isError && <p>Something terrible just happened: {error.message}</p>}
-      {isLoading && <p>loading</p>}
-      {isSuccess && <Gallery photos={photos} />}
-      <ImageUpload />
-    </Wrapper>
-  )
+    const isLoading = status === 'loading'
+    const isError = status === 'error'
+    const isSuccess = status === 'success'
+
+    return (
+        <Wrapper>
+            <AppHeader/>
+            {isError && <p>Something terrible just happened: {error.message}</p>}
+            {isLoading && <p>loading</p>}
+            {isSuccess && <Gallery photos={photos}/>}
+            <ImageUpload reloadPhotos={loadPhotos}/>
+        </Wrapper>
+    )
 }
 
 const Wrapper = styled.div`
--- frontend/src/components/ImageUpload.js
+++ frontend/src/components/ImageUpload.js
@@ -1,12 +1,42 @@
 import styled from 'styled-components/macro'
+import {useRef, useState} from "react";
+import axios from "axios";
 
-export function ImageUpload() {
-  return (
-    <Wrapper>
-      <input type="file" />
-      <button>Upload</button>
-    </Wrapper>
-  )
+export function ImageUpload({reloadPhotos}) {
+
+    const inputRef = useRef()
+    const [loading, setLoading] = useState()
+    const [error, setError] = useState()
+
+    const handleUpload = (event) => {
+        event.preventDefault()
+        setError()
+        const file = inputRef.current.files[0]
+        const formData = new FormData()
+        formData.set('image', file)
+
+        setLoading(true)
+        axios.post('/photo/upload', formData)
+            .then(res => res.data)
+            .then(() => reloadPhotos())
+            .catch(error => setError(error))
+            .finally(() => setLoading(false))
+    }
+
+    return (
+        <Wrapper onSubmit={handleUpload}>
+            {loading ?
+                <span>Loading...</span>
+                :
+                <>
+                    <input type="file" ref={inputRef}/>
+                    <button>Upload</button>
+                </>
+            }
+            {error && <span>Error: {error.message}</span>}
+
+        </Wrapper>
+    )
 }
 
 const Wrapper = styled.form`
